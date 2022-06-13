8af910ea6b06fe332a97abfafee2054ce2f72820
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-13 14:56:50 +0300

    commit tree d6437f60f56b60e4b38fd45224b80fda57270542

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+// function addingEventListener ()
+// {
+//     const input = document.getElementById("input");
+
+//     function clickAlert() {
+//       alert("I was clicked!");
+//     }
+
+//     input.addEventListener("click", clickAlert);
+// }
 function addingEventListener ()
 {
-    const input = document.getElementById("input");
-
-    function clickAlert() {
-      alert("I was clicked!");
-    }
-
-    input.addEventListener("click", clickAlert);
+  const input = document.getElementById("input");
+  function clickAlert() {
+    alert("I was clicked!");
+  }
+  input.addEventListener("click", clickAlert);
 }
+
